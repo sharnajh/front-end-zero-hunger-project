2c53212c6bb164897330527854a5383607cbb186
ladyomega <68204412+ladyomega@users.noreply.github.com>
2021-05-03 17:22:15 -0400

    change 3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-<<<<<<< Updated upstream
 import React, { useEffect } from "react";
-import logo from './logo.svg';
-import 'src/App.scss';
+import './App.css';
+import Navbar from './components/navbar/navbar.js';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Pages/home.js';
+import About from './Pages/about.js';
+import Services from './Pages/services.js';
+
+
 import {
   firestore
 } from "./firebase/firebase.js";
@@ -32,36 +37,7 @@ const App = () => {
       })
     // console.log();
   })
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
-import React from 'react';
-import 'src/App.css';
-import Navbar from 'src/components/navbar/navbar.js';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from 'src/Pages/home.js';
-import About from 'src/Pages/about.js';
-import Services from 'src/Pages/services.js';
-import SignIn from 'src/Pages/signIn.js';
 
-
-
-function App() {
   return (
     <Router>
       <Navbar />
@@ -69,10 +45,8 @@ function App() {
         <Route path='/' exact component={Home} />
         <Route path='/about' component={About} />
         <Route path='/services' component={Services} />
-        <Route path='/signIn' component={SignIn} />
       </Switch>
     </Router>
->>>>>>> Stashed changes
   );
 }
 
